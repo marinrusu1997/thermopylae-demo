7fab3525c26fe81012f0015a4dcd95bebe52fd7b
Rusu Marin <dimarusu2000@gmail.com>
2021-06-30 20:37:49 +0300

    change password

--- js/api.js
+++ js/api.js
@@ -31,9 +31,25 @@ const METHODS = {
         method: 'get',
         path: '/api/auth/session/active',
     },
+    GET_SUCCESSFUL_AUTHENTICATIONS: {
+        method: 'get',
+        path: '/api/auth/successful/attempts',
+    },
+    GET_FAILED_AUTHENTICATIONS: {
+        method: 'get',
+        path: '/api/auth/failed/attempts',
+    },
+    CHANGE_PASSWORD: {
+        method: 'put',
+        path: '/api/auth/account/password',
+    },
     LOGOUT: {
         method: 'delete',
         path: '/api/auth/session/logout',
+    },
+    LOGOUT_ONE: {
+        method: 'delete',
+        path: '/api/auth/session/logout/one',
     },
     LOGOUT_ALL: {
         method: 'delete',
@@ -79,6 +95,38 @@ const METHODS = {
  *	telephone: string,
  *	pubKey: string | undefined
  * }} RegistrationInfo
+ */
+
+/**
+ * @typedef {{
+ *     ip: string,
+ *     device: object | null | undefined,
+ *     location: object | null | undefined,
+ *     createdAt: number,
+ *     expiresAt: number
+ * }} UserSessionMetaData
+ */
+
+/**
+ * @typedef {{
+ *      id: string,
+ *      accountId: string,
+ *      ip: string,
+ *      device: object | null | undefined,
+ *      location: object | null | undefined,
+ *      authenticatedAt: number
+ * }} SuccessfulAuthenticationModel
+ */
+
+/**
+ * @typedef {{
+ *      id: string,
+ *      accountId: string,
+ *      ip: string,
+ *      device: object | null | undefined,
+ *      location: object | null | undefined,
+ *      detectedAt: number
+ * }} FailedAuthenticationModel
  */
 
 /**
@@ -367,6 +415,9 @@ class Api {
         throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
     }
 
+    /**
+     * @returns {Promise<object>}
+     */
     async getActiveSessions() {
         if (!STORAGE.hasJwt()) {
             if (!(await this.refreshUserSession())) {
@@ -382,7 +433,236 @@ class Api {
             referrerPolicy: 'no-referrer'
         });
 
+        if (response.status === 200) {
+            return await response.json();
+        }
 
+        if (response.status === 500) {
+            throw new ApiError('INTERNAL_SERVER_ERROR','Internal server error.');
+        }
+
+        if (response.status === 400) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'CSRF_HEADER_REQUIRED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'ACCESS_TOKEN_REQUIRED') {
+                throw new ApiError(body.error.code, 'Please retry operation.');
+            }
+        }
+
+        if (response.status === 401) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_SESSION') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
+    }
+
+    /**
+     * @returns {Promise<SuccessfulAuthenticationModel[]>}
+     */
+    async getSuccessfulAuthentications() {
+        if (!STORAGE.hasJwt()) {
+            if (!(await this.refreshUserSession())) {
+                throw new ApiError('INVALID_SESSION', 'Session expired.');
+            }
+        }
+
+        const response = await fetch(new URL(METHODS.GET_SUCCESSFUL_AUTHENTICATIONS.path, BASE_URL).href, {
+            method: METHODS.GET_SUCCESSFUL_AUTHENTICATIONS.method,
+            headers: {
+                'X-Requested-With': 'XmlHttpRequest'
+            },
+            referrerPolicy: 'no-referrer'
+        });
+
+        if (response.status === 200) {
+            return await response.json();
+        }
+
+        if (response.status === 500) {
+            throw new ApiError('INTERNAL_SERVER_ERROR','Internal server error.');
+        }
+
+        if (response.status === 400) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_INPUT') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'CSRF_HEADER_REQUIRED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'ACCESS_TOKEN_REQUIRED') {
+                throw new ApiError(body.error.code, 'Please retry operation.');
+            }
+        }
+
+        if (response.status === 401) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_SESSION') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
+    }
+
+    /**
+     * @returns {Promise<FailedAuthenticationModel[]>}
+     */
+    async getFailedAuthentications() {
+        if (!STORAGE.hasJwt()) {
+            if (!(await this.refreshUserSession())) {
+                throw new ApiError('INVALID_SESSION', 'Session expired.');
+            }
+        }
+
+        const response = await fetch(new URL(METHODS.GET_FAILED_AUTHENTICATIONS.path, BASE_URL).href, {
+            method: METHODS.GET_FAILED_AUTHENTICATIONS.method,
+            headers: {
+                'X-Requested-With': 'XmlHttpRequest'
+            },
+            referrerPolicy: 'no-referrer'
+        });
+
+        if (response.status === 200) {
+            return await response.json();
+        }
+
+        if (response.status === 500) {
+            throw new ApiError('INTERNAL_SERVER_ERROR','Internal server error.');
+        }
+
+        if (response.status === 400) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_INPUT') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'CSRF_HEADER_REQUIRED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'ACCESS_TOKEN_REQUIRED') {
+                throw new ApiError(body.error.code, 'Please retry operation.');
+            }
+        }
+
+        if (response.status === 401) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_SESSION') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
+    }
+
+    /**
+     * @param {string}  oldPassword
+     * @param {string}  newPassword
+     * @returns {Promise<void>}
+     */
+    async changePassword(oldPassword, newPassword) {
+        if (!STORAGE.hasJwt()) {
+            if (!(await this.refreshUserSession())) {
+                throw new ApiError('INVALID_SESSION', 'Session expired.');
+            }
+        }
+
+        const response = await fetch(new URL(METHODS.CHANGE_PASSWORD.path, BASE_URL).href, {
+            method: METHODS.CHANGE_PASSWORD.method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XmlHttpRequest'
+            },
+            body: JSON.stringify({ oldPassword, newPassword }),
+            referrerPolicy: 'no-referrer'
+        });
+
+        if (response.status === 204) {
+            return;
+        }
+
+        if (response.status === 500) {
+            throw new ApiError('INTERNAL_SERVER_ERROR','Internal server error.');
+        }
+
+        if (response.status === 400) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_INPUT') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'INCORRECT_PASSWORD') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'SIMILAR_PASSWORDS') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'WEAK_PASSWORD') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'CSRF_HEADER_REQUIRED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'ACCESS_TOKEN_REQUIRED') {
+                throw new ApiError(body.error.code, 'Please retry operation.');
+            }
+        }
+
+        if (response.status === 401) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_SESSION') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        if (response.status === 404) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'ACCOUNT_NOT_FOUND') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        if (response.status === 410) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'ACCOUNT_DISABLED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+        }
+
+        throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
     }
 
     /**
@@ -435,6 +715,64 @@ class Api {
 
             if (body.error.code === 'INVALID_SESSION') {
                 throw new ApiError(body.error.code, 'Please retry logout operation.');
+            }
+        }
+
+        throw new ApiError('MISCONFIGURATION', `Unable to interpret server response. Status: ${response.status}.`);
+    }
+
+    /**
+     * @param {string} refreshToken
+     * @returns {Promise<void>}
+     */
+    async logoutOne(refreshToken) {
+        if (!STORAGE.hasJwt()) {
+            if (!(await this.refreshUserSession())) {
+                throw new ApiError('INVALID_SESSION', 'Session expired.');
+            }
+        }
+
+        const response = await fetch(new URL(METHODS.LOGOUT_ONE.path, BASE_URL).href, {
+            method: METHODS.LOGOUT_ONE.method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XmlHttpRequest'
+            },
+            body: JSON.stringify({ 'refresh-token': refreshToken }),
+            referrerPolicy: 'no-referrer'
+        });
+
+        if (response.status === 204) {
+            return;
+        }
+
+        if (response.status === 500) {
+            throw new ApiError('INTERNAL_SERVER_ERROR','Internal server error.');
+        }
+
+        if (response.status === 400) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_INPUT') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'CSRF_HEADER_REQUIRED') {
+                throw new ApiError(body.error.code, body.error.message);
+            }
+
+            if (body.error.code === 'ACCESS_TOKEN_REQUIRED') {
+                throw new ApiError(body.error.code, 'Please retry operation.');
+            }
+        }
+
+        if (response.status === 401) {
+            /** @type {ErrorBody} */
+            const body = await response.json();
+
+            if (body.error.code === 'INVALID_SESSION') {
+                throw new ApiError(body.error.code, body.error.message);
             }
         }
 
--- pages/main/index.js
+++ pages/main/index.js
@@ -1,5 +1,6 @@
 import { API } from '../../js/api.js';
 import { PAGES } from '../../js/contants.js';
+import { ApiError } from "../../js/api-error.js";
 
 (function ($) {
     "use strict";
@@ -7,25 +8,153 @@ import { PAGES } from '../../js/contants.js';
     $('#successful-authentications-link').on('click', e => {
         e.preventDefault();
 
-        $('#successful-authentications-table').css('display', '');
-        $('#failed-authentications-table').css('display', 'none');
-        $('#active-sessions-table').css('display', 'none');
+        API.getSuccessfulAuthentications()
+            .then(authentications => {
+                const successfulAuthenticationsTable = $('#successful-authentications-table');
+                const successfulAuthenticationsTableBody = successfulAuthenticationsTable.find('tbody');
+
+                successfulAuthenticationsTableBody.empty();
+
+                for (let i = 0; i < authentications.length; i++) {
+                    successfulAuthenticationsTableBody.append(
+                        $('<tr>')
+                            .append($('<th>').attr('scope', 'row').text(String(i + 1)))
+                            .append($('<th>').text(authentications[i].ip))
+                            .append($('<td>').text(JSON.stringify(authentications[i].device)))
+                            .append($('<td>').text(JSON.stringify(authentications[i].location)))
+                            .append($('<td>').text(new Date(authentications[i].authenticatedAt * 1000).toISOString()))
+                    );
+                }
+
+                successfulAuthenticationsTable.css('display', '');
+                $('#failed-authentications-table').css('display', 'none');
+                $('#active-sessions-table').css('display', 'none');
+            }).catch(handleApiError);
     });
 
     $('#failed-authentications-link').on('click', e => {
         e.preventDefault();
 
-        $('#successful-authentications-table').css('display', 'none');
-        $('#failed-authentications-table').css('display', '');
-        $('#active-sessions-table').css('display', 'none');
+        API.getFailedAuthentications()
+            .then(authentications => {
+                const failedAuthenticationsTable = $('#failed-authentications-table');
+                const failedAuthenticationsTableBody = failedAuthenticationsTable.find('tbody');
+
+                failedAuthenticationsTableBody.empty();
+
+                for (let i = 0; i < authentications.length; i++) {
+                    failedAuthenticationsTableBody.append(
+                        $('<tr>')
+                            .append($('<th>').attr('scope', 'row').text(String(i + 1)))
+                            .append($('<th>').text(authentications[i].ip))
+                            .append($('<td>').text(JSON.stringify(authentications[i].device)))
+                            .append($('<td>').text(JSON.stringify(authentications[i].location)))
+                            .append($('<td>').text(new Date(authentications[i].detectedAt * 1000).toISOString()))
+                    );
+                }
+
+                $('#successful-authentications-table').css('display', 'none');
+                failedAuthenticationsTable.css('display', '');
+                $('#active-sessions-table').css('display', 'none');
+            }).catch(handleApiError);
     });
 
     $('#active-sessions-link').on('click', e => {
         e.preventDefault();
 
-        $('#successful-authentications-table').css('display', 'none');
-        $('#failed-authentications-table').css('display', 'none');
-        $('#active-sessions-table').css('display', '');
+        API.getActiveSessions()
+            .then(sessions => {
+                const activeSessionsTable = $("#active-sessions-table");
+                const activeSessionsTableBody = activeSessionsTable.find('tbody');
+
+                activeSessionsTableBody.empty();
+                for (const [/** @type {string} */ refreshToken, /** @type {UserSessionMetaData} */ session] of Object.entries(sessions)) {
+                    const tableRow = $('<tr>')
+                        .append($('<th>').attr('scope', 'row').text(session.ip))
+                        .append($('<td>').text(JSON.stringify(session.device)))
+                        .append($('<td>').text(JSON.stringify(session.location)))
+                        .append($('<td>').text(new Date(session.createdAt * 1000).toISOString()))
+                        .append($('<td>').text(new Date(session.expiresAt * 1000).toISOString()))
+                        .append($('<td>').append(
+                            $('<button>')
+                                .attr('type', 'button')
+                                .attr('class', 'btn btn-danger btn-sm px-3')
+                                .append(
+                                    $('<i>').attr('class', 'fas fa-times')
+                                )
+                                .on('click', e => {
+                                    e.preventDefault();
+
+                                    API.logoutOne(refreshToken)
+                                        .then(() => {
+                                            toastr.success('Session logged out');
+                                            tableRow.remove();
+                                        }).catch(handleApiError);
+                                })
+                        ));
+
+                    activeSessionsTableBody.append(tableRow);
+                }
+
+                activeSessionsTable.css('display', '');
+                $('#successful-authentications-table').css('display', 'none');
+                $('#failed-authentications-table').css('display', 'none');
+            })
+            .catch(handleApiError);
+    });
+
+    $('#change-password-link').on('click', e => {
+        e.preventDefault();
+
+        $('#change-password-modal').modal('toggle');
+    });
+    $('#change-password-modal-btn').on('click', async () => {
+        /* GET VALUES */
+        const oldPassword = $('#change-password-old-password-input').val().trim();
+        const newPassword = $('#change-password-new-password-input').val().trim();
+        const confirmPassword = $('#change-password-new-password-confirm-input').val().trim();
+
+        /* VALIDATE THEM */
+        if (!oldPassword) {
+            return toastr.warning('Please enter old password');
+        }
+        if (oldPassword.length < 14) {
+            return toastr.warning('Old password needs to contain at least 14 characters');
+        }
+        if (oldPassword.length > 4096) {
+            return toastr.warning('Old password needs to contain no more than 4096 characters');
+        }
+
+        if (!newPassword) {
+            return toastr.warning('Please enter new password');
+        }
+        if (newPassword.length < 14) {
+            return toastr.warning('New password needs to contain at least 14 characters');
+        }
+        if (newPassword.length > 4096) {
+            return toastr.warning('New password needs to contain no more than 4096 characters');
+        }
+
+        if (!confirmPassword) {
+            return toastr.warning('Please enter new password confirmation');
+        }
+        if (newPassword !== confirmPassword) {
+            return toastr.warning('Confirmed password does not match new password');
+        }
+
+        /* CHANGE PASSWORD */
+        runWaitMe('change-password-form');
+        API.changePassword(oldPassword, newPassword)
+            .then(() => {
+                toastr.success('Password has been changed.');
+                $('#change-password-modal').modal('hide');
+
+                setTimeout(() => {
+                    window.location.href = PAGES.LOGIN;
+                }, 1000);
+            })
+            .catch(handleApiError)
+            .finally(() => stopWaitMe('change-password-form'));
     });
 
     $('#logout-link').on('click', e => {
@@ -48,11 +177,36 @@ import { PAGES } from '../../js/contants.js';
                 toastr.success(`Logged out from ${response.numberOfDeletedSessions} sessions.`);
                 setTimeout(() => {
                     window.location.href = PAGES.LOGIN;
-                }, 2000);
+                }, 1000);
             })
             .catch(e => {
                 toastr.error(e.message, e.code || 'Error');
             });
     });
+
+    /* UTILS */
+    function runWaitMe(formId) {
+        $(`#${formId}`).waitMe({
+            effect: 'roundBounce',
+            text: '',
+            bg: 'rgba(255,255,255,0.7)',
+            color: '#000',
+            waitTime: -1
+        });
+    }
+
+    function stopWaitMe(formId) {
+        $(`#${formId}`).waitMe('hide');
+    }
+
+    function handleApiError(e) {
+        toastr.error(e.message, e.code || 'Error');
+
+        if (e instanceof ApiError && e.code === 'INVALID_SESSION') {
+            setTimeout(() => {
+                window.location.href = PAGES.LOGIN;
+            }, 1000);
+        }
+    }
 
 })(jQuery);
